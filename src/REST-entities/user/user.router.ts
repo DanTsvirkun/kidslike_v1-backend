56caafced593e265d1b0eb2659f88683e7bf78a7
DanTsvirkun <danila.tsvirkun555@gmail.com>
2021-01-18 18:15:44 +0200

    user router quick fix

--- src/REST-entities/user/user.router.ts
+++ src/REST-entities/user/user.router.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import { authorize } from "./../../auth/auth.controller";
 import tryCatchWrapper from "../../helpers/function-helpers/try-catch-wrapper";
-import { getAllInfo } from "./user.controller";
+import { getAllInfo, enGetAllInfo, plGetAllInfo } from "./user.controller";
 
 const router = Router();
 
 router.get("/info", tryCatchWrapper(authorize), tryCatchWrapper(getAllInfo));
-router.get("/info-en", tryCatchWrapper(authorize), tryCatchWrapper(getAllInfo));
-router.get("/info-pl", tryCatchWrapper(authorize), tryCatchWrapper(getAllInfo));
+router.get(
+  "/info-en",
+  tryCatchWrapper(authorize),
+  tryCatchWrapper(enGetAllInfo)
+);
+router.get(
+  "/info-pl",
+  tryCatchWrapper(authorize),
+  tryCatchWrapper(plGetAllInfo)
+);
 
 export default router;
